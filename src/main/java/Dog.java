ecd1b4aa0b4045f53e48c63db9b122c67cff370e
Jeremy-Lu-APComp <21jlu@athenian.org>
2020-10-30 21:09:14 +0000

    WIP Pet Inheritance HW

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,32 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog(String a, int b, String c) {
+        super(a,b);
+        this.breed = c;
+    }
+
+    public Dog() {
+        super("Steve", 4);
+        this.breed = "Pug";
+    }
 
     // makeNoise() method
 
 
     // toString method
 
+    public String toString(){
+        String str = this.getName() + " " + this.getAge() + " " + this.getBreed();
+        return str;
+    }
 
     // Getter
-
+    public String getBreed() {
+        return this.breed;
+    }
 
     // Setter
 }

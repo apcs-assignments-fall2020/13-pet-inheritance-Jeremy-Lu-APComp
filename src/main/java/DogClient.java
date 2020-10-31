c61d12690ff9d8620eeb21f714ae81e14f408da0
Jeremy-Lu-APComp <21jlu@athenian.org>
2020-10-31 16:26:29 +0000

    Final code for Pet Inheritance Homework

--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,9 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog d1 = new Dog("Rex", 4, "Pug");
+        System.out.println(d1); //tests toString method
+        System.out.println(d1.getBreed()); //tests getBreed method
+        d1.makeNoise(); //tests makeNoise method
+        System.out.println(new Dog()); //tests default constructor
     }
 }
